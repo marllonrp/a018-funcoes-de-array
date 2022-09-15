4f2d6211b2c19310882a087cb6b6a5aeed63490c
marllonrp <marllonrp@yahoo.com.br>
2022-09-15 14:02:04 -0300

    Atividades diárias.

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,34 @@
 const objeto = {
-    nome: "astrodev",
-    profissao: "Dev das estrelas",
-    username: "astrodev_labenu",
-    senha: "melhorDeTodos"
+  nome: "astrodev",
+  profissao: "Dev das estrelas",
+  username: "astrodev_labenu",
+  senha: "melhorDeTodos",
+};
+
+let caixaAlta = (objeto) => {
+  objeto.nome = objeto.nome.toUpperCase();
+  objeto.profissao = objeto.profissao.toUpperCase();
+  objeto.username = objeto.username.toUpperCase();
+  objeto.senha = objeto.senha.toUpperCase();
+  return objeto;
+};
+
+
+const textoCorrido = (objeto) => {
+  return (
+    objeto.nome +
+    " " +
+    objeto.profissao +
+    " " +
+    objeto.username +
+    " " +
+    objeto.senha
+  );
+};
+
+const imprimir = (objeto, funcao) => {
+    console.log(funcao(objeto) )
 }
+
+imprimir (objeto,caixaAlta)
+imprimir (objeto, textoCorrido)
